6bd2e1db4f1edff45fbbcea9edd9a34df8e912b9
sanketfulpagare2 <sanketfulpagare2@gmail.com>
2024-06-30 15:32:53 +0530

    new

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -3,7 +3,7 @@ import OpenAI from 'openai';
 
 
 const openai = new OpenAI({
-  apiKey:"my api key", // This is the default and can be omitted
+  apiKey:"My API Key", // This is the default and can be omitted
   dangerouslyAllowBrowser:true,
 });
 
